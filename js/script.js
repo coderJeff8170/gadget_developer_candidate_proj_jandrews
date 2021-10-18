0a1b06927b0bcac4508051973fb0ef72e3f31afc
Jeffrey Andrews <jeffrey.andrews@cognizant.com>
2021-10-18 15:35:49 -0400

    execution notes added to script file

--- js/script.js
+++ js/script.js
@@ -3,30 +3,14 @@
 
 /**
  * NOTE: in order to run simply from HTML file, this project has been refactored to avoid the use of modules
- * The classes are imported as scripts directly into the index.html file instead of as dependencies in modules where utilized
+ * The classes are imported as scripts directly into the index.html file instead of as dependencies in modules where utilized,
  * to avoid the need to run a local server or encounter cors issues.
- * If the user would like to run the project as modules on a local server such as live server, 
- * please switch to the branch 
- * 
- * 
- * 
- * 
+ * If the user would like to run the project as intended, (modules on a local server such as live server), 
+ * please switch to the git branch 'master' and run from there.
  */
 
 const DATA_URL = "data/data.json";
 const PANEL_DISPLAY = document.getElementById("content");
-
-// fetch(DATA_URL)
-//   .then((res) => res.json())
-//   .then((data) => {
-//     data.sections.map((itemData, index) => {
-//       const panel = new Panel(index, itemData);
-//       PANEL_DISPLAY.appendChild(panel);
-//     });
-//   })
-//   .catch((e) => {
-//     console.log(e.stack);
-//   });
 
 data.map((itemData, index) => {
         const panel = new Panel(index, itemData);
